ff06875d267794b265407d6f08b767a115740f25
Alexey <alexey.anasov@yandex.by>
2024-09-12 19:15:44 +0300

    шапка, навигация сайта

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-  /* Call plugins hrere ...*/
+  //slide2id - плавная прокрутка по ссылкам внутри страницы
+  $("a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id(
+    {
+      highlightSelector: "a",
+    }
+  );
 });
