d2f6d3d7aecc7a3db7bb351fd15132f2e5484c51
alexey <alexey.anasov@yandex.by>
2025-04-02 21:23:39 +0300

    анимация бургер-иконки

--- js/navigation.js
+++ js/navigation.js
@@ -1,37 +1,32 @@
 $(document).ready(function () {
-  var navToggleButton = $(".navigation__toggle");
+  // Опеделяем переменные
+  var navToggleButton = $("#navigation__button");
   var navToggleIcon = $(".navigation__toggle .fa");
   var navBlock = $(".navigation__list");
   var navBlockOpen = "navigation__list--open";
   var navLink = $(".navigation__list a");
-  var iconNav = "fa-bars";
-  var iconClose = "fa-times";
 
-  // Мобильная навигация
+  // События по клику на иконку
   navToggleButton.on("click", function (e) {
     e.preventDefault();
     navBlock.toggleClass(navBlockOpen);
-
-    if (navToggleIcon.hasClass(iconNav)) {
-      navToggleIcon.removeClass(iconNav);
-      navToggleIcon.addClass(iconClose);
-    } else {
-      navToggleIcon.removeClass(iconClose);
-      navToggleIcon.addClass(iconNav);
-    }
+    navButtonToggle();
   });
 
+  // События по клику на ссылки
   navLink.on("click", function () {
     if (navBlock.hasClass(navBlockOpen)) {
-      if (navToggleIcon.hasClass(iconNav)) {
-        navToggleIcon.removeClass(iconNav);
-        navToggleIcon.addClass(iconClose);
-      } else {
-        navToggleIcon.removeClass(iconClose);
-        navToggleIcon.addClass(iconNav);
-      }
+      navButtonToggle();
     }
-
     navBlock.removeClass(navBlockOpen);
   });
+
+  // Функция для анимации иконки
+  function navButtonToggle() {
+    if (navToggleButton.hasClass("active")) {
+      navToggleButton.removeClass("active");
+    } else {
+      navToggleButton.addClass("active");
+    }
+  }
 });
