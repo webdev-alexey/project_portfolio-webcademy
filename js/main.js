f2fe3e0fd597199828aaab11e0c5e4ead7059440
alexey <alexey.anasov@yandex.by>
2025-04-03 20:26:07 +0300

    валидация формы jquery

--- js/main.js
+++ js/main.js
@@ -19,4 +19,25 @@ $(document).ready(function () {
     },
   });
   // End of FancyBox - gallery
+
+  // jQuery Validate JS
+  $("#contact-form").validate({
+    rules: {
+      name: { required: true },
+      email: { required: true, email: true },
+      // skype:  { required: true },
+      // phone:  { required: true },
+      message: { required: true },
+    },
+
+    messages: {
+      name: "Пожалуйста, введите свое имя",
+      email: {
+        required: "Пожалуйста, введите свой email",
+        email:
+          "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой.",
+      },
+      message: "Пожалуйста, введите текст сообщения",
+    },
+  });
 });
