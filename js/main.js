06fc72859eed3fd3ed72f9da7459bc59e0fae476
alexey <alexey.anasov@yandex.by>
2025-04-04 21:30:11 +0300

    отправка формы php

--- js/main.js
+++ js/main.js
@@ -39,5 +39,29 @@ $(document).ready(function () {
       },
       message: "Пожалуйста, введите текст сообщения",
     },
+    submitHandler: function (form) {
+      ajaxFormSubmit();
+    },
   });
+
+  // Функция AJAX запрса на сервер
+  function ajaxFormSubmit() {
+    var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+    // Формируем ajax запрос
+    $.ajax({
+      type: "POST", // Тип запроса - POST
+      url: "php/mail.php", // Куда отправляем запрос
+      data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+      // Функция если все прошло успешно
+      success: function (html) {
+        $("#contact-form").slideUp(800);
+        $("#answer").html(html);
+      },
+    });
+
+    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+    return false;
+  }
 });
